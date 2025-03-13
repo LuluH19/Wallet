c06446c6fb9d9504f0db0dd007b1c8a69b43fcba
Vivien-Parsis <vivienparsis.perso@gmail.com>
2025-03-13 16:28:46 +0100

    added create virement

--- back-end/src/route/virement.route.js
+++ back-end/src/route/virement.route.js
@@ -22,8 +22,54 @@ virementRouter.get("/", (req, res) => {
                 } else if (compte.utilisateur != currentUser._id) {
                     return res.status(400).send({ message: "not your compte" })
                 } else {
-                    virementModel.find({expediteur:currentCompte._id}).populate({ path: "expediteur", select: "numeroCompte" }).then(virements => {
+                    virementModel.find({ expediteur: currentCompte._id }).populate({ path: "expediteur", select: "numeroCompte" }).then(virements => {
                         return res.status(200).send(virements)
+                    })
+                }
+            })
+        }
+    })
+})
+
+virementRouter.post("/create", (req, res) => {
+    const token = req.headers.authorization || ""
+    const currentUser = decodeToken(token)
+    const currentVirement = {
+        expediteur: req.body.expediteur || "",
+        destinataire: req.body.destinataire || "",
+        montant: req.body.montant || "",
+        devise: req.body.devise || "EUR",
+        date: req.body.date || new Date()
+    }
+    if (!isValidDataObject(currentVirement)) {
+        return res.status(400).send({ message: "virement missing" })
+    }
+    userModel.findOne(currentUser).then(user => {
+        if (!user) {
+            return res.status(400).send({ message: "user not found" })
+        } else {
+            compteModel.findOne({ _id: currentVirement.expediteur }).then(expediteur => {
+                if (!expediteur) {
+                    return res.status(400).send({ message: "expediteur not found" })
+                } else if (expediteur.utilisateur != currentUser._id) {
+                    return res.status(400).send({ message: "not your compte" })
+                } else {
+                    compteModel.findOne({ _id: currentVirement.destinataire }).then(destinataire => {
+                        if (!destinataire) {
+                            return res.status(400).send({ message: "destinataire not found" })
+                        } else {
+                            if (currentVirement.montant > expediteur.solde) {
+                                return res.status(400).send({ message: "not enough money to make the virement" })
+                            } else {
+                                compteModel.findOneAndUpdate({ utilisateur: currentVirement.expediteur }, { solde: expediteur.solde - currentVirement.montant }).then(() => {
+                                    compteModel.findOneAndUpdate({ utilisateur: currentVirement.destinataire }, { solde: destinataire.solde + currentVirement.montant }).then(() => {
+                                        virementModel.create(currentVirement).then(()=>{
+                                            res.status(200).send({ message: "virement created" })
+                                        })
+                                    })
+                                })
+                            }
+                        }
                     })
                 }
             })
